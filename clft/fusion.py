f838b57db3ecabd7a3b3e7bb4c53f10d717dafcf
김진언 <rlawlsdjs4582@gmail.com>
2026-01-25 19:42:27 +0900

    SRCU update

--- clft/fusion.py
+++ clft/fusion.py
@@ -28,7 +28,7 @@ class ResidualConvUnit(nn.Module):
 
 
 class Fusion(nn.Module):
-    def __init__(self, resample_dim, alpha = 0.10):
+    def __init__(self, resample_dim, alpha=0.15):
         super(Fusion, self).__init__()
         #self.res_conv1 = ResidualConvUnit(resample_dim)
         self.res_conv_xyz = ResidualConvUnit(resample_dim)
@@ -43,20 +43,40 @@ class Fusion(nn.Module):
         # C: (B,D,H,W), L': (B,D,H,W), M: (B,1,H,W)  -> in_ch = 2D+1
         self.gate_conv = nn.Conv2d(2 * resample_dim + 1, 1, kernel_size=1, bias=True)
 
+        # logging
+        # self.last_log = {
+        #     "g": None,
+        #     "scale": None
+        # }
+    
+
     def forward(self, rgb, lidar, previous_stage=None, modal = 'rgb', mask=None):
         if previous_stage == None:
                 previous_stage = torch.zeros_like(rgb)
 
         if modal == 'rgb':
-            output_stage1_rgb = self.res_conv_rgb(rgb)
-            output_stage1_lidar = torch.zeros_like(output_stage1_rgb)
+            # 기존
+            # output_stage1_rgb = self.res_conv_rgb(rgb)
+            # output_stage1_lidar = torch.zeros_like(output_stage1_rgb)
+            
+            # gated fusion
+            C = self.res_conv_rgb(rgb)
+            Fused = C
+
         if modal == 'lidar':
-            output_stage1_lidar = self.res_conv_xyz(lidar)
-            output_stage1_rgb = torch.zeros_like(output_stage1_lidar)
+            # 기존
+            # output_stage1_lidar = self.res_conv_xyz(lidar)
+            # output_stage1_rgb = torch.zeros_like(output_stage1_lidar)
+            
+            # gated fusion
+            Lp = self.res_conv_xyz(lidar)
+            Fused = Lp
+
         if modal == 'cross_fusion': 
             # output_stage1_rgb = self.res_conv_rgb(rgb)
             # output_stage1_lidar = self.res_conv_xyz(lidar)
 
+            # gated fusion
             C  = self.res_conv_rgb(rgb)
             Lp = self.res_conv_xyz(lidar)
 
@@ -71,7 +91,14 @@ class Fusion(nn.Module):
             g = torch.sigmoid(self.gate_conv(gate_in))     # (B,1,H,W)
 
             scale = self.alpha + (1.0 - self.alpha) * g    # (B,1,H,W)
-            Fused = C + scale * (mask * Lp)                  
+            Fused = C + scale * (mask * Lp)       
+
+            # logging
+            # if self.training:  # train 때만
+            #     with torch.no_grad():
+            #         # scalar로 저장 (gpu tensor -> python float)
+            #         self.last_log["g"] = float(g.mean().item())
+            #         self.last_log["scale"] = float(scale.mean().item())
 
 
         # output_stage1 = output_stage1_lidar + output_stage1_rgb + previous_stage
